be43db3bbd6cb4f58cbac7d79944a939e5b4c7a2
Sergey <smchonin@gmail.com>
2020-03-05 03:43:38 +0300

    initial commit

---
+++ src/main/java/DataClasses.kt
@@ -0,0 +1,63 @@
+class TimeCode(var hours: Int, var minutes: Int, var seconds: Int, private val frames: Int){
+
+    fun substractTimeCode(valueToSubstract: TimeCode): TimeCode{
+        var newSeconds: Int
+        var newMinutes: Int
+        var newHours: Int
+
+        // frames
+        var newFrames = frames - valueToSubstract.frames
+        if (newFrames < 0){
+            newFrames = 50 - newFrames
+            seconds--
+        }
+        //seconds
+        newSeconds = seconds - valueToSubstract.seconds
+        if (newSeconds < 0){
+            newSeconds = 60 - newSeconds
+            minutes--
+        }
+        // minutes
+        newMinutes = minutes - valueToSubstract.minutes
+        if (newMinutes < 0){
+            newMinutes = 60 - newMinutes
+            hours--
+        }
+        // hours
+        newHours = hours - valueToSubstract.hours
+        if (newHours < 0){
+            newHours = 60 - newHours
+            println("Hour limit exceeded while substracting TimeCodes")
+        }
+        return TimeCode(newHours, newMinutes, newSeconds, newFrames)
+    }
+
+    override fun toString(): String {
+        return "${twoSymbolFormatting(hours)}:${twoSymbolFormatting(minutes)}:${twoSymbolFormatting(seconds)}:${twoSymbolFormatting(frames)}"
+    }
+
+    private fun twoSymbolFormatting(digits: Int): String {
+        var stringDigits = digits.toString()
+        if (stringDigits.length < 2) {
+            stringDigits = "0$stringDigits"
+        }
+        return stringDigits
+    }
+}
+
+data class Event(
+    val inPointStartTime: Long,
+    val outPointStartTime: Long,
+    val id: Int
+)
+
+data class Segment(
+    val timeStamp: String,
+    val videoStartTime: Long
+)
+
+data class EventBetweenSegments(
+    val event: Event,
+    val segmentFirstStart: Segment,
+    val segmentSecondStart: Segment
+)
---
+++ src/main/java/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val configuredFrames = 50
+    val xmlPath = "data/replay.xml"
+    println("vMixToAdobePremiere started")
+
+    // get all events
+
+    // get all segments
+
+    // match events between segments
+
+    // start writing
+
+}
